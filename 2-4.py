fac28fc0c01de484eb3f846ed6e26f3191fb6db0
0itsme0 <44812083+0itsme0@users.noreply.github.com>
2022-09-08 21:03:50 +0300

    Update 2-4.py

--- 2-4.py
+++ 2-4.py
@@ -1,6 +1,6 @@
-first_name = "vlad"
-second_name = "dementev"
-fullname = first_name + " " + second_name
-print(fullname.upper())
-print(fullname.lower())
-print(fullname.title())
+first_name = "vlad" # Вводим переменную с именем
+second_name = "dementev" # Вводим переменную с фамилией
+fullname = first_name + " " + second_name # Объединяем две переменные
+print(fullname.upper()) # Выводим имя большими буквами
+print(fullname.lower()) # Выводим имя маленькими буквами
+print(fullname.title()) # Выводим имя с капитализацией букв
