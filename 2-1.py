e0aad5d4e7b828238bdbd5ae4ab857cd8baff3ea
0itsme0 <44812083+0itsme0@users.noreply.github.com>
2022-11-10 10:27:53 +0300

    Update 2-1.py

--- 2-1.py
+++ 2-1.py
@@ -1,2 +1,10 @@
-message = "Hello ИСП-21" # Вводим переменную с нашим сообщением
-print(message) # Выводим сообщение
+class Person:
+  def __init__(self, name):
+    self.__name = name 
+    self.__age = 1
+    
+  def set_age(self, age):
+    if age in range(1, 100)
+     self.__age = age
+    else:
+      print('Недопустимый возраст')
